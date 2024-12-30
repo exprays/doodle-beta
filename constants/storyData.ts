5e517739165b0352d99ccfcea7c39052670bf1f3
Surya <suryakant.subudhi@outlook.com>
2024-12-30 20:37:53 +0530

    Update storyData.ts

--- constants/storyData.ts
+++ constants/storyData.ts
@@ -11,9 +11,9 @@ export const STORY_DATA: StoryContent[] = [
       React.createElement('b', { key: 'o' }, 'o'),
       'n'
     ]),
-    description: "A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure.",
+    description: "Everywhere we will go, we will have a lot of fun I swear!!:)",
     buttonText: "Read Summer Story",
-    fullStory: "Our summer adventures were filled with unexpected joy and countless memories..."
+    fullStory: "Remember our first meet? On the blue flag beach.... It was really an amazing moment for me 🥹! Two really random strangers meeting for first time, smiling and we didn't have anything to say! But... it worked! Sometimes I wonder what I did to deserve you, to deserve your love 🥹💗! Shit... I'm being sukumari in your words 😂! We will have a lot of fun punu... travelling the world together...eating a lot of veggies and much more! Just be with me....."
   },
   {
     id: "love",
@@ -26,20 +26,85 @@ export const STORY_DATA: StoryContent[] = [
     ]),
     description: "An anime and gaming-inspired NFT collection - the IP primed for expansion.",
     buttonText: "Read Love Story",
-    fullStory: "The journey of our love story began with a simple coffee date..."
+    fullStory: "Unexpected! Everything was so random 😂.... you are really an unexpected surprise:) The defining moment of my life for sure... because you know it! I was never so happy...:( You slammed me so hard and sent me to heavens 🥹! Everyday now I wake up ..... I choose you to be my first thought! and I will.... I thought love was never meant for me but after meeting you..... everything seems to be opposite:) The day we started talking, I had no idea this would be how it will turn... The best thing that happened to me is for sure YOU!💗 "
   },
   {
     id: "yummy",
     type: "video",
     src: "/videos/bakery.mp4",
     title: React.createElement('span', {}, [
-      'Lo',
-      React.createElement('b', { key: 'v' }, 'v'),
-      'e'
+      'Yu',
+      React.createElement('b', { key: 'm' }, 'm'),
+      'my'
     ]),
     description: "An anime and gaming-inspired NFT collection - the IP primed for expansion.",
     buttonText: "Read Love Story",
-    fullStory: "The journey of our love story began with a simple coffee date..."
+    fullStory: "My first date!! Yes .... and it was perfect 😂💗! Btw I have seen how much you eat.... literally starter finish heiparilani tama dwara 😄😂... and I forgot that I have not eaten anything! Reason? ............. Your eyes! Man ho raha tha bas dekhta rahoon... I saw my future in it.....to be with you all the time, and having fun... And I cook well too.... so don't worry! Obviously veg hin baneiba...😂!"
+  },
+  {
+    id: "crazy",
+    type: "video",
+    src: "/videos/happy.mp4",
+    title: React.createElement('span', {}, [
+      'Cr',
+      React.createElement('b', { key: 'a' }, 'a'),
+      'zy'
+    ]),
+    description: "An anime and gaming-inspired NFT collection - the IP primed for expansion.",
+    buttonText: "Read Love Story",
+    fullStory: "I have been a careless person all the time of my life.... never studied hard, never thought about consequences of anything what I do..... but I care for you 🥹! Despite of my day.... a snap of yours with your photo, a text from you made me smile like a crazy guy 😂! nd it will continue to happen..... Everytime you send me something :) You are the thing I want the most..... My everything 💗!"
+  },
+  {
+    id: "cute",
+    type: "video",
+    src: "/videos/alright.mp4",
+    title: React.createElement('span', {}, [
+      'C',
+      React.createElement('b', { key: 'u' }, 'u'),
+      'te'
+    ]),
+    description: "An anime and gaming-inspired NFT collection - the IP primed for expansion.",
+    buttonText: "Read Love Story",
+    fullStory: "Koi no yokan really! That feeling when I first saw you...... I knew I was gonna fall in love hard! Maybe I don't love you the way you need... but I do! I do love you more than anything.... Likhu toh lafztum ho, sochu toh khayal tum ho, maangu toh duaa tum ho, sach kahu.... toh mohabbat tum ho.... Actually irony eta ki such a cute girl falling for me? 😂😂... choice pua re bahut kharap na tamara... but chaliba ebe heisarilani ta kn au kariba? 😂😂😂... you have to live with it now :) Thanks.... for everything! Comfort me like this.... okay? "
+  },
+  {
+    id: "kiss",
+    type: "video",
+    src: "/videos/kisses.mp4",
+    title: React.createElement('span', {}, [
+      'K',
+      React.createElement('b', { key: 'i' }, 'i'),
+      'ss'
+    ]),
+    description: "The most intimate moments we share together are the ones that mean the most.",
+    buttonText: "Read Love Story",
+    fullStory: "I have never been so close to anyone.... I have never been so comfortable with anyone.... I have never been so happy with anyone.... I have never been so loved by anyone.... I have never been so cared by anyone.... I have never been so kissed to love....and it really feels amazing! You are the person I want to spend my life with.... I want to be with you all the time.... I want to be with you in every moment of my life.... I want to be with you in every situation of my life.... I want to be with you in every decision of my life.... I want to be with you in every happiness of my life.... I want to be with you in every sadness of my life.... I want to be with you in every success of my life! Sending you a lot of kisses and love!💗"
+  },
+  {
+    id: "hugs",
+    type: "video",
+    src: "/videos/hugs.mp4",
+    title: React.createElement('span', {}, [
+      'H',
+      React.createElement('b', { key: 'u' }, 'u'),
+      'gs'
+    ]),
+    description: "Hugs are the best way to show someone you care.",
+    buttonText: "Read Love Story",
+    fullStory: "Give me your bad days, give me your good ones too! Give me your happy and sad moments, give me your tears and smiles too! You have suffered a lot this time... and it really hurts! And it's not okay 🥹..but things are going to be better:) You know, in the kind of way where you just want to spend time with your favorite person? Where you know that no matter what happens, you got each other to get you through it. That the little nonverbals that we share, end up creating new memories that we can look back on and giggle at from time to time. The funny smirks, the subtle eye rolls or wide eyes. The belly rubs and the power naps from eating too much. When you realize you can look up and see your person from across the room, when something happens, instead of wanting to call... because you're in the same room. And the smile that stretches across our faces, the warmth that spreads through our chests, and the yearning to cross the room and experience it together, hand in hand. I want to take pictures and look through them together and talk about how much fun we had in those moments. I want your arms I'm around me, and mine around you. I want to be able to walk across the room and hold you, kiss your lips, and share those little moments with you. To hug you tight until you feel comfortable...that's all I can dream of:)"
+  },
+  {
+    id: "happy",
+    type: "video",
+    src: "/videos/funplay.mp4",
+    title: React.createElement('span', {}, [
+      'H',
+      React.createElement('b', { key: 'a' }, 'a'),
+      'ppy'
+    ]),
+    description: "Hugs are the best way to show someone you care.",
+    buttonText: "Read Love Story",
+    fullStory: "I want to be happy with you..and I just wanna make you the happiest you've ever been. I want to make you feel safe and loved and comforted. I want you to know that you can come to me when you're not ok, when your eyes are filled with tears and I'll hold you til you're a little more ok. I'm always here for you. I love you at your best and at your worst.Nothings gonna change that. Ours is one of those love stories ppl will talk abt for years to come bc we have something extraordinary, something real. You are not just my world, you're my everything. I'm here and I'm gonna keep being here every day for the rest of our lives. I'm not leaving, I'm not going anywhere, I'm staying right here with you. I wouldn't dream of being anywhere else. You truly are my home, my safe place, the love of my life. I want you to know that I sure as hell mean it when I say I'd do anything for you. I'd do absolutely anything. I love you now and I love you always. Everything is going to be okay. We are going to be okay. This will last as long as we choose each other and keep choosing each other for the rest our lives. And I have no doubt in my mind that we will. I love you. Really I do. With everything I have in me. I love you.💗"
   },
   {
     id: "text-story",
